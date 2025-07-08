6994be993c3543a781e8e8e707b12906e5d549ca
Gjermund Lunder <gjermund.lunder@borregaard.com>
2025-07-08 10:09:31 +0000

    new

---
+++ webapp/controller/FioriAppGjermund.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("fiorimodulegjermund.controller.FioriAppGjermund", {
+        onInit() {
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"fiorimodulegjermund/test/unit/controller/FioriAppGjermund.controller"
+], function () {
+	"use strict";
+});
